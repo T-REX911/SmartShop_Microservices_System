19e77c37ebbc345f8d58917e18ba466a49ec18e3
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-10-07 11:08:37 +0530

    Add Product

--- product-service/src/main/java/com/trex/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/trex/product_service/controller/ProductController.java
@@ -1,9 +1,27 @@
 package com.trex.product_service.controller;
 
+import com.trex.product_service.Dto.ProductDto;
+import com.trex.product_service.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    //add
+    @PostMapping("/add")
+    public ResponseEntity<?> addProduct(ProductDto dto){
+        return productService.addProduct(dto);
+    }
+    //update
+    //delete
+    //getById
+    //getALl
+    //getByName
 }
--- product-service/src/main/java/com/trex/product_service/service/ProductService.java
+++ product-service/src/main/java/com/trex/product_service/service/ProductService.java
@@ -1,7 +1,32 @@
 package com.trex.product_service.service;
 
+import com.trex.product_service.Dto.ProductDto;
+import com.trex.product_service.model.Product;
+import com.trex.product_service.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+    public ResponseEntity<?> addProduct(ProductDto dto) {
+        try{
+            Product product = new Product();
+            product.setName(dto.getName());
+            product.setQty(dto.getQty());
+            product.setPrice(dto.getPrice());
+
+            Product save = productRepository.save(product);
+            log.info("Product created : "+save.toString());
+            return new ResponseEntity<>(save, HttpStatus.CREATED);
+        }catch (Exception e){
+            log.error("Product not created : "+ e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_ACCEPTABLE);
+        }
+    }
 }
