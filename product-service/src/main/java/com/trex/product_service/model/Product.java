6d6e4b42e09a181176373a2682a7a16720047ae6
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-10-07 10:46:35 +0530

    Product Model Created

---
+++ product-service/src/main/java/com/trex/product_service/model/Product.java
@@ -0,0 +1,20 @@
+package com.trex.product_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int qty;
+    private double price;
+}
