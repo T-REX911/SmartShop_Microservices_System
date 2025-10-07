c41024961a307144722e398cb8bdbea4aaacb750
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-10-07 13:49:06 +0530

    order service created

---
+++ order-service/src/main/java/com/trex/order_service/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.trex.order_service.service;
+
+import com.trex.order_service.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    @Autowired
+    OrderRepository orderRepository;
+}
